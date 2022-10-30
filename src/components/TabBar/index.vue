d93a2cb97892d2b7b26fb30586db6f9495489708
TanchoRki <2932371726@qq.com>
2022-10-30 18:58:11 +0800

    2022-10-30

---
+++ src/components/TabBar/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <footer id="footer">
+    <ul>
+      <router-link to="/movie" custom v-slot="{ navigate,isActive }">
+        <li @click="navigate" :class="isActive?'activeRoute':''">
+          <span class="iconfont icon-quanpiao"></span>
+          <p>电影</p>
+        </li>
+      </router-link>
+
+      <router-link to="/cinema" custom v-slot="{ navigate,isActive }">
+        <li @click="navigate" :class="isActive?'activeRoute':''">
+          <span class="iconfont icon-shipin"></span>
+          <p>影院</p>
+        </li>
+      </router-link>
+
+      <router-link to="/mine" custom v-slot="{ navigate,isActive }">
+        <li @click="navigate" :class="isActive?'activeRoute':''">
+          <span class="iconfont icon-wode"></span>
+          <p>我的</p>
+        </li>
+      </router-link>
+    </ul>
+  </footer>
+</template>
+<script>
+export default {
+  name: 'TabBar'
+}
+</script>
+<style lang="scss" scoped>
+#footer {
+  width: 100%;
+  height: 50px;
+  background: white;
+  border-top: 2px #ebe8e3 solid;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  ul {
+    display: flex;
+    text-align: center;
+    height: 50px;
+    align-items: center;
+    li {
+      flex: 1;
+      span {
+        font-size: 20px;
+      }
+      p {
+        font-size: 12px;
+      }
+    }
+    li.activeRoute {
+      color: rgb(254, 165, 0);
+    }
+  }
+}
+</style>
---
+++ src/views/Movie/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="main">
+    <Header />
+    <tab-bar />
+  </div>
+</template>
+<script>
+import Header from '@/components/Header'
+import TabBar from '@/components/TabBar'
+export default {
+  name: 'Movie',
+  components: {
+    TabBar,
+    Header
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
